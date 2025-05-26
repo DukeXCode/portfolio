dd91e13bd79b7237c6fd358e48a49d8e2722d502
Duke <naim.giger@outlook.com>
2025-05-26 21:40:55 +0200

    Fix deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
 import en from '../src/translation/en.json';
 
-await i18n
+i18n
     .use(initReactI18next)
     .init({
         lng: 'en',
